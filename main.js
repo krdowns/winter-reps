61cbdf2a8a1e298e6fbe671666c844480bb681b2
krdowns <krjdowns16@gmail.com>
2018-12-30 10:56:30 -0800

    Section A. Done

---
+++ main.js
@@ -0,0 +1,18 @@
+// How do we assign a value to a variable?
+var variable = 1
+// How do we change the value of a variable?
+var variable = 2
+// How do we assign an existing variable to a new variable?
+var newVariable = variable
+// Remind me, what are declare, assign, and define?
+
+'Declare is creating a variable with a name'
+
+'Assign is to give that created variable a value'
+
+'Define is to create a variable and assign a value to it.'
+
+// What is pseudocoding and why should you do it?
+'It is using normal language to solve a problem before you use coding syntax to write the solution'
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+'I would say that at least 75% of the time should be spent planning so when you are coding you are just typing out your solution rather than solving it as you type.'
