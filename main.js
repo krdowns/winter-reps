bc7536e5dfa11ba26ab158a2fe85fd3119f49c13
krdowns <krjdowns16@gmail.com>
2018-12-30 11:02:54 -0800

    Section B Done

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+
+// SECTION A - Q & A //
+
 // How do we assign a value to a variable?
-var variable = 1
+var variable = 1;
 // How do we change the value of a variable?
-var variable = 2
+var variable = 2;
 // How do we assign an existing variable to a new variable?
-var newVariable = variable
+var newVariable = variable;
 // Remind me, what are declare, assign, and define?
 
 'Declare is creating a variable with a name'
@@ -16,3 +19,30 @@ var newVariable = variable
 'It is using normal language to solve a problem before you use coding syntax to write the solution'
 // What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
 'I would say that at least 75% of the time should be spent planning so when you are coding you are just typing out your solution rather than solving it as you type.'
+
+// SECTION B - STRINGS //
+
+// Create a variable called firstVariable.
+var firstVariable;
+
+// Assign it the value of the string "Hello World"
+firstVariable = "Hello World";
+
+// Change the value of this variable to some number.
+firstVariable = 2;
+
+// Store the value of firstVariable in a new variable called secondVariable
+var secondVariable = firstVariable;
+
+// Change the value of secondVariable to any string.
+secondVariable = "New string"
+
+// What is the value of firstVariable?
+2
+
+// Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+// ex: Hello, my name is Jean Valjean
+var yourName = "Kenny Downs"
+
+console.log("Hello my name is " + yourName);
+console.log(`Hello my name is ${yourName}`);
